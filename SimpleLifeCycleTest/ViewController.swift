857c84925784279132b3e780245e67bde9c1b264
Jaesuk <jaesuk95@outlook.com>
2023-09-09 18:45:13 +0900

    simple life cycle theory

--- SimpleLifeCycleTest/ViewController.swift
+++ SimpleLifeCycleTest/ViewController.swift
@@ -13,7 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("VC-1 view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("VC-1 view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("VC-1 View will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("VC-1 view Did disappear")
+    }
 
-
+    deinit {
+        print("VC-1 메모리에서 내려감")
+    }
 }
 
